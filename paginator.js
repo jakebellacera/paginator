cad87e4bf54dee36a2572ebd3c9f17c53f50d607
Jake Bellacera <hi@jakebellacera.com>
2012-11-06 12:51:19 -0800

    First commit!
    * Adding code
    * Adding readme

---
+++ paginator.js
@@ -0,0 +1,243 @@
+/**
+ * paginator.js
+ * ------------
+ * A superfast paginator, built in Prototype.
+ * 
+ * Usage:
+ *   paginated = new Paginator(myElement, options);
+ *   paginated.build();
+ * 
+ * Available methods:
+ *   build()     - Builds the layout and throws the element's children into individual pages
+ *   goToPage(n) - Go to a specific page
+ *   destroy()   - Reverts the paginated element back to its original state.
+ * 
+ * Available options and their defaults:
+ *   amount:   3
+ *   numbers:  true
+ *   paddles:  true
+ *   prevText: 'Previous'
+ *   nextText: 'Next'
+ * 
+ * Paginator also has callbacks available. These are options with a function as a value.
+ * Available callbacks:
+ *   onInvalidPage
+ * 
+ * Credits:
+ *   Author: Jake Bellacera (http://jakebellacera.com)
+ *   URL:    http://github.com/jakebellacera/paginator
+ */
+
+var Paginator = (function () {
+  var Page;
+
+  function Paginator(container, opts) {
+    // Override any default settings
+    this.settings = Object.extend({
+      amount: 3,
+      numbers: true,
+      paddles: true,
+      prevText: 'Previous',
+      nextText: 'Next',
+      onInvalidPage: function (n) {
+        alert('Went to invalid page:' + n);
+      }
+    }, opts);
+
+    // Set the container
+    this.container = container;
+  };
+
+  Paginator.prototype.build = function () {
+    var self = this;
+
+    // Unset destroyed status
+    self.built = true;
+
+    // Get the child elements
+    self.children = self.container.childElements();
+
+    // Pages accessor
+    self.pages = [];
+
+    // Set the current page to 0
+    self.curPage = 0;
+
+    // Create the pages' container
+    self.wrap = new Element('div', {'class': 'pagination-pages'});
+    self.container.insert(self.wrap);
+
+    // Build the pages
+    for (var i = 0; i < Math.ceil(self.children.length/self.settings.amount); i++) {
+      self.pages.push( new Page( new Element('div', { 'class': 'page-' + (i+1)}) ) );
+      self.wrap.insert(self.pages[i].node);
+
+      // Put scoped child elements into the page
+      self.children.slice(i * self.settings.amount, ((i+1) * self.settings.amount) || 9e9).each(function (child) {
+        self.pages[i].add(child);
+      });
+    }
+
+    // Build the navigation
+    self.createNav();
+
+    // Go to the first page
+    self.goToPage(self.curPage);
+
+    return self;
+  };
+
+  // Navigation builder
+  Paginator.prototype.createNav = function () {
+    var self = this, a;
+
+    if (self.built) {
+      // Create the navigation container
+      self.nav = new Element('div', { 'class': 'pagination-nav' });
+      self.container.insert(self.nav);
+
+      // Numbers navigation handler (1,2,3,4,etc)
+      if (self.settings.numbers === true) {
+        self.numbers = new Element('div', {'class': 'pagination-numbers'});
+
+        for (var i = 0; i < self.pages.length; i++) {
+          // Create a number URL
+          a = new Element('a', {'href': '#'}).insert(i+1);
+          a.on('click', function () {
+            if (!this.hasClassName('active')) {
+              self.goToPage(this.previousSiblings().length);
+            }
+            return false;
+          });
+          self.numbers.insert(a);
+        }
+
+        // Put numbers nav inside nav container
+        self.nav.insert(self.numbers);
+      }
+
+      // Paddle navigation handler (next/prev)
+      if (self.settings.paddles) {
+        // Prev button
+        self.prevPaddle = new Element('a', {'href': '#', 'class': 'pagination-paddle prev'}).insert(self.settings.prevText);
+        self.prevPaddle.on('click', function () {
+          if (!this.hasClassName('disabled')) {
+            self.goToPage(self.curPage - 1);
+          }
+        });
+
+        // Next button
+        self.nextPaddle = new Element('a', {'href': '#', 'class': 'pagination-paddle next'}).insert(self.settings.nextText);
+        self.nextPaddle.on('click', function () {
+          if (!this.hasClassName('disabled')) {
+            self.goToPage(self.curPage + 1);
+          }
+        });
+
+        // Append paddle buttons to the navigation container
+        self.nav.insert({
+          top: self.prevPaddle,
+          bottom: self.nextPaddle
+        });
+      }
+    } else {
+      throw("Paginator is not built. Please run build() before executing this method.");
+    }
+
+    return self;
+  }
+
+  // Page traversing handler
+  Paginator.prototype.goToPage = function (n) {
+    var self = this;
+
+    try {
+      if (self.built) {
+        self.pages[n].node.show().siblings().each(function (siblingPage) {
+          siblingPage.hide();
+        });
+
+        self.curPage = n;
+
+        // Handle number nav
+        if (self.settings.numbers) {
+          self.numbers.childElements()[self.curPage].addClassName('active').siblings().each(function (sibling) {
+            sibling.removeClassName('active');
+          });
+        }
+
+        // Handle paddle nav
+        if (self.settings.paddles) {
+          if (self.curPage === 0) {
+            self.prevPaddle.addClassName('disabled');
+          } else {
+            self.prevPaddle.removeClassName('disabled');
+          }
+
+          if (self.curPage === (self.pages.length - 1)) {
+            self.nextPaddle.addClassName('disabled');
+          } else {
+            self.nextPaddle.removeClassName('disabled');
+          }
+        }
+      } else {
+        throw("Error: Paginator is not built. Please run build() before executing this method.");
+      }
+
+    } catch (TypeError) {
+      self.settings.onInvalidPage(n);
+    }
+
+    return self;
+  }
+
+  // Completely remove Paginator
+  Paginator.prototype.destroy = function () {
+    var self = this;
+
+    if (self.built) {
+      self.pages.each(function (page) {
+        // Move elements back into container
+        page.destroy(self.container);
+      });
+
+      // remove wrapper
+      self.wrap.remove();
+
+      // Remove navigation
+      self.nav.remove();
+
+      // Set destroyed status
+      self.built = false;
+    } else {
+      throw("Error: Paginator is not built. Please run build() before executing this method.");
+    }
+
+    return self;
+  }
+
+  Page = (function () {
+    function Page(node) {
+      this.node = node.hide();
+      return this;
+    }
+
+    // Add a child element
+    Page.prototype.add = function (ele) {
+      return this.node.insert(ele);
+    };
+
+    // Move all child elements into a new location
+    Page.prototype.destroy = function (newLocation) {
+      var self = this;
+
+      self.node.remove().childElements().each(function (child) {
+        newLocation.insert(child);
+      });
+    };
+
+    return Page;
+  })();
+
+  return Paginator;
+})();
---
+++ readme.md
@@ -0,0 +1,99 @@
+# paginator.js
+
+A superfast paginator, built in Prototype.
+
+## Usage
+
+```javascript
+Paginator(myElement, options);
+```
+
+To page an element's children, simply pass the element to a new Paginator object.
+
+```javascript
+myElement = document.getElementById('myElement');
+paginator = new Paginator(myElement);
+paginator.build();
+```
+
+## Options
+
+Available options and their defaults for Paginator are:
+
+```javascript
+options = {
+  amount: 3,            // Number of items per page
+  numbers: true,        // Show numbers navigation
+  paddles: true,        // Show next/prev navigation
+  prevText: 'Previous', // Text for previous paddle
+  nextText: 'Next'      // Text for next paddle
+}
+```
+
+You can also set options after Paginator has been built via the `settings` hash:
+
+```javascript
+paginated = paginator(myElement);
+
+paginated.settings.amount = 3;
+paginated.settings.numbers = true;
+paginated.settings.paddles = true;
+paginated.settings.prevText = 'Previous';
+paginated.settings.nextText = 'Next';
+```
+
+### Callbacks
+
+Paginator supports callbacks. A callback is a function that is fired after a specific event. There is currently only one callback. You set callbacks in the options hash.
+
+#### onInvalidPage(_pageNumber[int]_)
+
+```javascript
+{
+  onInvalidPage: function (pageNumber) {
+    alert('Went to invalid page:' + pageNumber);
+  }
+}
+
+`onInvalidPage` is fired when the user goes to an invalid page. This should never happen and is installed for precautionary reasons.
+
+## Methods
+
+Paginator currently supports three methods: `build`, `goToPage` and `destroy`.
+
+### build
+
+Builds and binds events to the paginator. Returns the `Paginator` instance. `build` cannot be ran once it's built.
+
+```javascript
+paginated = paginator(myElement);
+paginated.build();
+```
+
+### goToPage(_pageNumber[int]_)
+
+Tells the Paginator instance to go to a specific page. If the page number is out of bounds, the `onInvalidPage` callback will fire. Returns the `Paginator` instance.
+
+_Note: `goToPage` requires the Paginator instance to be built. Run `build` prior to using this method._
+
+```javascript
+paginated = paginator(myElement);
+paginated.build();
+paginated.goToPage(5);
+```
+
+### destroy
+
+Reverts the `Paginator` instance back to the visual state prior to being built.
+
+_Note: An unbuilt `Paginator` instance cannot be destroyed._
+
+```javascript
+paginated = paginator(myElement);
+paginated.build();
+paginated.destroy();
+```
+
+## Contributing
+
+Find a bug? Want to add a feature? Fork this repo and then submit a pull request. :thumbs:
